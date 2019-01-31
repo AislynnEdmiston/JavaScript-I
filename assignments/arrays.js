ae5c84731d97d7c9b49c2c2808e00992837b5f2d
Aislynn E <davidandaislynn@gmail.com>
2019-01-30 22:56:20 -0500

    objects.js completed.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -69,8 +69,8 @@ console.log(`Car 33 is a *car year goes here* *car make goes here* *car model go
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 0;
-console.log();
+let lastCar = inventory[inventory.length - 1];
+console.log(`${lastCar.car_make} ${lastCar.car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
@@ -90,7 +90,8 @@ console.log();
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
-console.log();
+JSON.stringify(BMWAndAudi);
+console.log(JSON.stringify(BMWAndAudi));
 
 
 
--- assignments/objects.js
+++ assignments/objects.js
@@ -28,7 +28,10 @@ const interns = [
     id: 2,
     email: 'kdiben1@tinypic.com',
     name: 'Kennan',
-    gender: 'M'
+    gender: 'M',
+    speak: function speak(){
+      return "Hello, my name is Kennan!";
+    }
   },
   {
     id: 3,
@@ -36,8 +39,6 @@ const interns = [
     name: 'Keven',
     gender: 'M'
   },
-  // 4,gmartinson3@illinois.edu,Gannie,M
-// 5,adaine5@samsung.com,Antonietta,F
   {
     id: 4,
     email: 'gmartin3@illinois.edu',
@@ -48,7 +49,10 @@ const interns = [
     id: 5,
     email: 'adaine5@samsung.com',
     name: 'Antonietta',
-    gender: 'F'
+    gender: 'F',
+    multipyNums: function (x, y){
+      return x*y;
+    }
   }
 ]
 
@@ -59,21 +63,26 @@ const interns = [
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(interns[0].name);
 // Kennan's ID
-
+console.log(interns[1].id);
 // Keven's email
-
+console.log(interns[2].email);
 // Gannie's name
-
+console.log(interns[3].name);
 // Antonietta's Gender
+console.log(interns[4].gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+console.log(interns[1].speak())
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+console.log(interns[4].multipyNums(3,4))
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
